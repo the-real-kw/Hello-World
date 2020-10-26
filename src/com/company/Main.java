8988a6355c088cee3f1de90ed666ced2e35d4e82
Kevin Withers <kevin.withers999@gmail.com>
2020-10-26 17:43:32 -0400

    Revert "Commiting branch 100"
    This reverts commit ed9442b89e4d419c374bca2ded25b2fc37b1e772.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,6 @@ public class Main {
         System.out.println("making changes to third branch");
         System.out.println("This is a fifth branch");
         System.out.println("testing out a new branch");
-        System.out.println("This is Branch 100");
 
         Employees emp1 = new Employees("1234", "Kevin");
         System.out.println(emp1.getEmpName());
